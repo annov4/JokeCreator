ce6c8217c0fdcba36e4c747af554edff79ab5383
annov4 <137298843+annov4@users.noreply.github.com>
2024-03-05 22:07:51 +0300

    second commit

--- src/JokeCreator.java
+++ src/JokeCreator.java
@@ -1,32 +1,44 @@
-import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.FileVisitor;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.attribute.BasicFileAttributes;
 
 public class JokeCreator {
-    public static void getFiles(File dirName) throws IOException { //dirName - корневая папка для поиска
-        if (dirName.isDirectory()) { //является ли dirName папкой
-            File[] files = dirName.listFiles(); //получим все файлы, которые лежат внутри папки
-            if (files != null) {
-                for (File file : files) { //проверяем каждый файл
-                    if (file.isDirectory()) { // если файл является папкой
-                        getFiles(file); // функция вызывает сама себя
-                        addJokeFile(file);// жобавляем в каждую подпапку joke.java
-                    }
-                }
+    public static void getFiles(Path dirPath) throws IOException { //Path.dir коренная папка
+        Files.walkFileTree(dirPath, new FileVisitor<Path>() { //обрабатываем дерево каталогов
+            @Override
+            public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException { //включает в себя набор методов, которые следует выполнить перед посещением текущей поддиректории.
+                return FileVisitResult.CONTINUE;// Чтобы исследовать каталог необходимо вернуть FileVisitResult.CONTINUE
             }
-        }
+
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException { //набор методов, которые следует выполнить во время посещения текущего файла
+                return FileVisitResult.CONTINUE;
+            }
+
+            @Override
+            public FileVisitResult visitFileFailed(Path file, IOException exc) { //Данный метод может пригодиться при ошибке доступа к файлу
+                return FileVisitResult.CONTINUE;
+            }
+
+            @Override
+            public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException { //все, что необходимо сделать после посещения директории, нужно перечислить в рамках перегрузки данного метода.
+                addJokeFile(dir);//добавляем файл joke.java в каждую папку
+                return FileVisitResult.CONTINUE;
+            }
+
+        });
     }
 
-    public static void addJokeFile(File dirName) throws IOException {
-        File jokeFile = new File(dirName, "joke.java");
-        //нужно ли проверять, есть ли этот файл уже в данной папке?
-        //if (!jokeFile.exists()) {
-        jokeFile.createNewFile();
-        FileWriter fileWriter = new FileWriter(jokeFile);
-        fileWriter.write("public class Joke " +
-                    "{\npublic static void main(String[] args) " +
-                    "{\n System.out.println(\"Hello, World!\");\n}\n}");
-        fileWriter.flush();
-        fileWriter.close();
+    public static void addJokeFile(Path dirPath) throws IOException {
+        Path path = dirPath.resolve("joke.java");//преобразует последовательность путей или сегментов путей в абсолютный путь
+        Files.createFile(path);
+        Files.write(path, ("public class Joke " +
+                "{\n public static void main(String[] args) " +
+                "{\n System.out.println(\"Hello, World!\");\n}\n}").getBytes());
+
     }
 }
+
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.file.Path;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String dirName = reader.readLine();// считываем путь к папке с консоли
-        File folder = new File(dirName);
-        JokeCreator.getFiles(folder);
+        Path dirPath = Path.of(dirName);
+        JokeCreator.getFiles(dirPath);
     }
 }
 /*
